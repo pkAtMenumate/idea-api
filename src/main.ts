041eca9045c9d4b1eba1a102b2d81fe607cef7e1
menumate <prem.kumar@menumatetech.com>
2019-11-05 11:31:44 +0530

    port changed and .env created..

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import "dotenv/config";
+import {Logger} from '@nestjs/common';
 
+const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Listning at : http://localhost:3000');
+  await app.listen(port);
+  Logger.log(`Server Running At : http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
